cb0e4f81f87d4fc70afa13f3874f0d39c79df6ca
Roger <rcornejom@unemi.edu.ec>
2025-11-23 13:17:04 -0500

    sssss

--- auth-project/backend/server.js
+++ auth-project/backend/server.js
@@ -64,6 +64,6 @@ app.use('/api/usuarios', verificarToken, usuariosRoutes);
 
 // Puerto
 const port = process.env.PORT || 5000;
-app.listen(port, "0.0.0.0", () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
